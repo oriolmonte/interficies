f1d3d46d8b26489608a3fab50c2cdf7642348896
Oriol Monte <oriol.monte95@gmail.com>
2024-10-21 20:55:50 +0200

    21

--- navegacio/app/src/main/java/cat/oriol/navegacio/pantalles/CaraOCreu.kt
+++ navegacio/app/src/main/java/cat/oriol/navegacio/pantalles/CaraOCreu.kt
@@ -1,9 +1,32 @@
 package cat.oriol.navegacio.pantalles
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import cat.oriol.navegacio.R
 
+@Preview
 @Composable
 fun PantallaCaraOcreu()
 {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    )
+    {
+        Image(
+            painter = painterResource(id= R.drawable.cara)
+            ,contentDescription = "creu"
+        )
+    }
+
 
 }
